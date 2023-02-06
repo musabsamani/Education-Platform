62b8d4b5c82d25d65b7d3b3ef68cdae655796a44
Musab Obada Y. Elsamani <musab0124887085@gmail.com>
2023-02-06 14:36:02 +0200

    comments file added

--- server/model/model.js
+++ server/model/model.js
@@ -1,12 +1,25 @@
 const mongoose = require("mongoose");
-var schema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
+var user = new mongoose.Schema({
+  name: String,
   age: String,
   address: String,
   phone: String,
 });
-const Userdb = mongoose.model("userdb", schema);
-module.exports = Userdb;
+var volunteer = new mongoose.Schema({
+  name: String,
+  // age: String,
+  // specialization: String,
+  email: String,
+  address: String,
+  phone: String,
+  subject: String
+});
+var table = new mongoose.Schema({
+  name: String,
+  age: String,
+  address: String,
+  phone: String,
+});
+const Userdb = mongoose.model("userdb", user);
+const Volunteerdb = mongoose.model("volunteerdb", volunteer);
+const Tabledb = mongoose.model("tabledb", table);
