1364098957a3c857c6c77d801a3b4da221c7c7d2
Mr-Som3a <mrsom3a8@gmail.com>
2023-02-06 14:21:48 +0300

    up

--- src/component/show.jsx
+++ src/component/show.jsx
@@ -9,7 +9,13 @@ const Show=(user)=> {
                 <div className="container mt-5 ">
                 <h2>Voulunteer Information</h2>
                 <form className="col g-3">
-                    <h1>hello</h1>
+                    <Input type="text" name="name" label="Name" value={user.name} />
+                    <Input type="text" name="specializaiton" label="Pro-Specialization" value={user.specializaiton} />
+                    <Input type="text" name="subject" label="Subject" value={user.subject} />
+                    <Input type="email" name="email" label="Email" value={user.email} />
+                    <Input type="text" name="address" label="Address" value={user.address} />
+                    <Input type="text" name="phone" label="Phone" value={user.phone} />
+                    
                         <div className="col mt-2">
                             <Link to="/classes"><button className="btn btn-success m-1">Back</button></Link>
                         </div>
