f47880f4f3481453a6abd73aa6805e3a12e16865
Mr-Som3a <mrsom3a8@gmail.com>
2023-02-06 21:02:34 +0300

    i

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -11,9 +11,9 @@ const Home = () => {
                 <Link to='/addvolunteer'><button className="btn btn-outline-primary m-3">Sign as Volunteer</button></Link>
                 <Link to='/addstudent'><button className="btn btn-outline-primary m-3">Sign as Student</button></Link>
                 <Link to='/studentTable'><button className="btn btn-outline-info m-3 ">Our Student</button></Link>
-                <Link to='/class'><button className="btn btn-outline-success m-3 ">Classes</button></Link>
+                <Link to='/classes'><button className="btn btn-outline-success m-3 ">Classes Table</button></Link>
                 <Link to='/addsubject'><button className="btn btn-outline-success m-3 ">Add Subject</button></Link>
-                <Link to='/classes'><button className="btn btn-outline-info m-3 ">Our Classes</button></Link>
+                <Link to='/oursubject'><button className="btn btn-outline-info m-3 ">Our Subject</button></Link>
                 {/* <Link to='/Calendar'><button className="btn btn-outline-info m-3">Calendar</button></Link> */}
             </div>
         </>
