5882ce272c6814e3a942ba1446149e6e34f1942b
Musab Obada Y. Elsamani <musab0124887085@gmail.com>
2023-02-06 20:27:01 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -42,11 +42,7 @@ class App extends Component {
       this.setState({ table })
       const { data: subjects } = await axios.get(subjectAPI)
       this.setState({ subjects })
-      console.log(studentAPI)
-      console.log(volunteerAPI)
-      console.log(tableAPI)
-      console.log(subjectAPI)
-      // console.log("this.state")
+      console.log(this.state)
 
     } catch {
       console.log("error fetching data from the server")
