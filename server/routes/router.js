f7ce74bda94b6f86c88e5a321d11474515ccc59c
Musab Obada Y. Elsamani <musab0124887085@gmail.com>
2023-02-06 16:15:39 +0200

    updated

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const route = express.Router();
 const services = require("../services/render");
-const controller = require("../controllers/controller");
+const controller = require("../controllers/students");
 /**
  * @description Root Route
  * @method GET /
  */
 route.get("/", services.homeRoutes);
 /**
- * @description update user
+ * @description update student
  * @method GET /
  */
-route.get("/update_user", services.updateUser);
-route.get("/add_user", services.addUser);
+route.get("/update_student", services.updateStudent);
+route.get("/add_student", services.addStudent);
 
 // API
-route.post("/api/users", controller.create);
-route.get("/api/users", controller.find);
-route.put("/api/users/:id", controller.update);
-route.delete("/api/users/:id", controller.delete);
+route.post("/api/students", controller.create);
+route.get("/api/students", controller.find);
+route.put("/api/students/:id", controller.update);
+route.delete("/api/students/:id", controller.delete);
 
 module.exports = route;
