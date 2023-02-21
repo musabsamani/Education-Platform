b8049487ceb21d8874cba82cb696987b30965d98
Musab Obada Y. Elsamani <musab0124887085@gmail.com>
2023-02-22 00:36:18 +0200

    update apiEndpoints

--- src/helpers/apiEndpoints.js
+++ src/helpers/apiEndpoints.js
@@ -1,7 +1,7 @@
-const studentAPI = "http://localhost:5000/api/students"
-const volunteerAPI = "http://localhost:5000/api/volunteers"
-const lessonAPI = "http://localhost:5000/api/lessons"
-const subjectAPI = "http://localhost:5000/api/subjects"
 const baseAPI = "http://localhost:5000/api"
+const studentAPI = `${baseAPI}/students`
+const volunteerAPI = `${baseAPI}/volunteers`
+const lessonAPI = `${baseAPI}/lessons`
+const subjectAPI = `${baseAPI}/subjects`
 
 export { studentAPI, volunteerAPI, lessonAPI, subjectAPI, baseAPI }
