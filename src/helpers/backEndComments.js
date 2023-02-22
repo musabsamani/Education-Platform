9ffb730c0289f5fad9bf3e8ee723cd6dcdf03b78
Musab Obada Y. Elsamani <musab0124887085@gmail.com>
2023-02-22 13:10:47 +0200

    backEnd Comment file added

--- src/helpers/backEndComments.js
+++ src/helpers/backEndComments.js
@@ -1,4 +1,7 @@
-// ? =============================
-// ? =====  BACK-END COMMENTS HERE
-// ? =============================
-"/src/asses"
+// ? =====  Not Emergent
+// 01 - for more Organizing component and consistent file structure
+/*
+    convert imgae on "/src/component/show/download.png" to the path
+    "/src/assets/img"
+*/
+// ? =====  Emergent !!!!
