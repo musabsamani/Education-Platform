b0453b76303fa63700bca87f129f6db1d3822e26
Musab Obada Y. Elsamani <musab0124887085@gmail.com>
2023-03-03 00:56:20 +0200

    up

---
+++ server/helpers/fileSystemPathes.js
@@ -0,0 +1,6 @@
+
+// profile cover file paths // root/upload
+const uploadBasePath = "uploads"
+// profile cover file paths // root/uploadBasePath/profileCovers
+const profileCoverBasePath = "profileCovers"
+module.exports = { uploadBasePath, profileCoverBasePath }
