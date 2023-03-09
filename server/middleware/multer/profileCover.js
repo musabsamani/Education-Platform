42a5c68a89a739d752dcbfdbfb092eabeafbc070
musabobada <musab012488708@gmail.com>
2023-03-09 16:32:29 +0200

    gitIgnore

--- server/middleware/multer/profileCover.js
+++ server/middleware/multer/profileCover.js
@@ -1,6 +1,5 @@
 const multer = require("multer");
 const path = require("path");
-const { removeFile } = require("../../helpers/helpersFunction");
 const { uploadBasePath, profileCoverBasePath } = require("../../helpers/fileSystemPathes");
 const profileCoverUploadPath = path.join(uploadBasePath, profileCoverBasePath);
 let imageMimeTypes = ["image/jpeg", "image/png", "image/gif", "image/bmp", "image/tiff", "image/svg+xml", "image/webp"];
