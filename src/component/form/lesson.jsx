fe07655a0b2cbef7e2544611d10cbd5d38c7ef38
musabobada <musab012488708@gmail.com>
2023-03-27 22:20:25 +0200

    up

--- src/component/form/lesson.jsx
+++ src/component/form/lesson.jsx
@@ -10,7 +10,7 @@ class Lesson extends Component {
           <form className="col g-3 d-flex-column justify-content-center" id={`${this.props.name}Lesson`} onSubmit={this.props.name === "add" ? (e) => this.props.create(e, "addLesson", "lessons") : (e) => this.props.update(e, "updateLesson", "lessons")}>
             <div className="col-md-5">
               {this.props.subjects.length > 0 ? (
-                <select onChange={this.props.onChange} value={this.props.temporary.subject} name="subject" className=" ml-3 mt-3 form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+                <select onChange={this.props.onChange} value={this.props.temporary.subjectCode} name="subjectCode" className=" ml-3 mt-3 form-select form-select-lg mb-3" aria-label=".form-select-lg example">
                   {this.props.subjects.map((subject) => (
                     <option key={Math.random()} value={subject.value}>
                       {subject.code}
@@ -23,24 +23,13 @@ class Lesson extends Component {
                 </select>
               )}
             </div>
-            <PopupSubject subject={this.props.subjects} />
             <div className="col-md-5">
-              {this.props.volunteers.length > 0 ? (
-                <select onChange={this.props.onChange} value={this.props.temporary.volunteer} name="volunteer" className=" ml-3 mt-3 form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-                  {this.props.volunteers.map((volunteer) => (
-                    <option key={Math.random()} value={volunteer.value}>
-                      {volunteer.name}
-                    </option>
-                  ))}
-                </select>
-              ) : (
-                <select disabled className=" ml-3 mt-3 form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-                  <option>Volunteers Database is Empty</option>
-                </select>
-              )}
+              <Input type="text" name="name" label="Name" onChange={this.props.onChange} value={this.props.temporary.name} />
+            </div>
+            <div className="col-md-5">
+              <Input type="text" name="content" label="Content" onChange={this.props.onChange} value={this.props.temporary.content} />
             </div>
             {this.props.name === "add" ? "" : <Input type="hidden" name="_id" value={this.props.temporary._id} />}
-            <Input type="date" name="date" label="Date" onChange={this.props.onChange} value={this.props.formater(this.props.temporary.date)} />
             <div className="s col mt-2">
               <button className="btn btn-primary m-1" type="submit">
                 {this.props.name == "add" ? "Submit" : "Save"}
--- src/component/show/studentTable.jsx
+++ src/component/show/studentTable.jsx
@@ -8,65 +8,65 @@ class StudentTable extends Component {
     return (
       <>
         <div className='main'>
-                <Sidebar/>
-                <div className='content'>
-                    <Adminbar/>
-                    <hr />
-                    <div className="center">
-                    <div className="tb container mt-5 ">
-          <Link to="/addStudent">
-            <button className="btn btn-outline-primary m-3" onClick={() => this.props.setTemporaryEmpty()}>
-              Add Student
-            </button>
-          </Link>
-          <table className="table caption-top mt-5">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Name</th>
-                <th scope="col">Age</th>
-                <th scope="col">Address</th>
-                <th scope="col">Phone</th>
-                <th scope="col">Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.props.students.length > 0 ? (
-                this.props.students.map((student, i) => (
-                  <tr key={student._id}>
-                    <td>{i + 1}</td>
-                    <td>{student.name}</td>
-                    <td>{student.age}</td>
-                    <td>{student.address}</td>
-                    <td>{student.phone}</td>
-                    <td>
-                      <Link to={`/updateStudent?id=${student._id}`}>
-                        <button className="btn btn-info btn-sm" onClick={() => this.props.setTemporary(student)}>
-                          Update
-                        </button>
-                      </Link>
+          <Sidebar />
+          <div className='content'>
+            <Adminbar />
+            <hr />
+            <div className="center">
+              <div className="tb container mt-5 ">
+                <Link to="/addStudent">
+                  <button className="btn btn-outline-primary m-3" onClick={() => this.props.setTemporaryEmpty()}>
+                    Add Student
+                  </button>
+                </Link>
+                <table className="table caption-top mt-5">
+                  <thead>
+                    <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">Name</th>
+                      <th scope="col">Age</th>
+                      <th scope="col">Address</th>
+                      <th scope="col">Phone</th>
+                      <th scope="col">Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {this.props.students.length > 0 ? (
+                      this.props.students.map((student, i) => (
+                        <tr key={student._id}>
+                          <td>{i + 1}</td>
+                          <td>{student.name}</td>
+                          <td>{student.age}</td>
+                          <td>{student.address}</td>
+                          <td>{student.phone}</td>
+                          <td>
+                            <Link to={`/updateStudent?id=${student._id}`}>
+                              <button className="btn btn-info btn-sm" onClick={() => this.props.setTemporary(student)}>
+                                Update
+                              </button>
+                            </Link>
 
-                      <button className="btn btn-danger btn-sm" onClick={() => this.props.onDelete(student._id, "students")}>
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                ))
-              ) : (
-                <tr>
-                  <td>Databse is Empty</td>
-                </tr>
-              )}
-            </tbody>
-          </table>
-        </div>
-                    </div>  
-                    <div className="bottom">
-    
-                    </div>  
-                </div>
+                            <button className="btn btn-danger btn-sm" onClick={() => this.props.onDelete(student._id, "students")}>
+                              Delete
+                            </button>
+                          </td>
+                        </tr>
+                      ))
+                    ) : (
+                      <tr>
+                        <td>Databse is Empty</td>
+                      </tr>
+                    )}
+                  </tbody>
+                </table>
+              </div>
             </div>
-        
+            <div className="bottom">
+
+            </div>
+          </div>
+        </div>
+
       </>
     );
   }
