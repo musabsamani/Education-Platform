4360cf5801d9b2fe9bd4b019892198230a63831d
Mr-Som3a <mrsom3a8@gmail.com>
2023-04-14 23:36:12 +0300

    up

---
+++ src/component/lessonDetails.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class LessonDetails extends Component {
+    state = {}
+    render() {
+        return (
+            <>
+                {/* start header */}
+                <header>
+
+                </header>
+                {/* end header */}
+                {/* start main */}
+                <main>
+
+                </main>
+                {/* end main */}
+                {/* start footer */}
+                <footer>
+
+                </footer>
+                {/* end footer */}
+            </>
+        );
+    }
+}
+
+export default LessonDetails;
---
+++ src/testdesign.jsx
@@ -0,0 +1,66 @@
+// import React, { Component } from 'react';
+
+// class TestDesign extends Component {
+//     state = {}
+//     render() {
+//         return (
+//             <>
+//                 <div className="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" style="width: 280px;">
+//                     <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+//                         <svg className="bi pe-none me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+//                         <span className="fs-4">Sidebar</span>
+//                     </a>
+//                     <hr>
+//                         <ul className="nav nav-pills flex-column mb-auto">
+//                             <li className="nav-item">
+//                                 <a href="#" className="nav-link active" aria-current="page">
+//                                     <svg className="bi pe-none me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
+//                                     Home
+//                                 </a>
+//                             </li>
+//                             <li>
+//                                 <a href="#" className="nav-link link-body-emphasis">
+//                                     <svg className="bi pe-none me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
+//                                     Dashboard
+//                                 </a>
+//                             </li>
+//                             <li>
+//                                 <a href="#" className="nav-link link-body-emphasis">
+//                                     <svg className="bi pe-none me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
+//                                     Orders
+//                                 </a>
+//                             </li>
+//                             <li>
+//                                 <a href="#" className="nav-link link-body-emphasis">
+//                                     <svg className="bi pe-none me-2" width="16" height="16"><use xlink:href="#grid"></use></svg>
+//                                     Products
+//                                 </a>
+//                             </li>
+//                             <li>
+//                                 <a href="#" className="nav-link link-body-emphasis">
+//                                     <svg className="bi pe-none me-2" width="16" height="16"><use xlink:href="#people-circle"></use></svg>
+//                                     Customers
+//                                 </a>
+//                             </li>
+//                         </ul>
+//                         <hr>
+//                             <div className="dropdown">
+//                                 <a href="#" className="d-flex align-items-center link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+//                                     <img src="https://github.com/mdo.png" alt="" width="32" height="32" className="rounded-circle me-2" />
+//                                     <strong>mdo</strong>
+//                                 </a>
+//                                 <ul className="dropdown-menu text-small shadow">
+//                                     <li><a className="dropdown-item" href="#">New project...</a></li>
+//                                     <li><a className="dropdown-item" href="#">Settings</a></li>
+//                                     <li><a className="dropdown-item" href="#">Profile</a></li>
+//                                     <li><hr className="dropdown-divider" /></li>
+//                                     <li><a className="dropdown-item" href="#">Sign out</a></li>
+//                                 </ul>
+//                             </div>
+//                         </div>
+//                     </>
+//                     );
+//     }
+// }
+
+//                     export default TestDesign;
